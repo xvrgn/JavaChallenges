5c23dd41a61bfa269f36af4e5a416ef50c053f22
Brent Xavier Agno <agnobrentxavier462@gmail.com>
2025-02-26 22:06:11 +0800

    Revert "added a comments about the algorithm of the reverseWord method"
    This reverts commit 815304b22639d97c2d6b7380eebf0a3f65d87389.

--- src/challenges/WordReversal.java
+++ src/challenges/WordReversal.java
@@ -1,14 +1,6 @@
 package challenges;
 
 public class WordReversal {
-
-    /**
-     * Initialize a variable: Create an empty string {@code output} to hold the reversed keyword;
-     * Loop through input String: for each character of input word;
-     * Reverse the string: concatenate each character {@code ch} in the beginning of {@code output} String;
-     * Return the reversed word: after the loop finishes, the {@code output} contains the reversed word of the {@code input} String
-     */
-
     public static String reverseWord(String input) {
         char ch;
         String output = "";
